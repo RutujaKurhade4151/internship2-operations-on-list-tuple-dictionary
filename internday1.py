4f0d8abae0a0cf0b07e066fa224ef2aa0a7e83e7
RutujaKurhade4151 <151777880+RutujaKurhade4151@users.noreply.github.com>
2024-07-03 00:38:20 +0530

    Update internday1.py

--- internday1.py
+++ internday1.py
@@ -21,35 +21,3 @@ print(mylist[2])
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#dictionary{}
-
-#['gender'] = 'male'
-#del mydict['age']
-#mydict['city'] = 'mumbai
-
-
-#tuple() immutable=do not modify
-
-#mytup(1,2,3)
-#newel=4
-#newtup = mytup + (newel,)#add
-#newtup = mytup[ :1] + mytup[ :2] #remove
